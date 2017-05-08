4900db1d30ffc2c04779013126158944526e6b52
Dan Kruchinin <dkruchinin@acm.org>
2017-05-08 11:10:12 +0100

    Release: 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sanic-prometheus',
-    version='0.1.2',
+    version='0.1.3',
     description='Exposes various prometheus monitoring metrics of Sanic-based apps.',
     url='http://github.com/dkruchinin/sanic-prometheus',
     author='Dan Kruchinin',
