695971de2b2f3d01c56614a17af4437ec9d47fe5
Altan Alpay <altan.alpay@gmail.com>
2023-03-08 21:40:03 -0800

    Fix compatibility issues with Sanic 22.12.0+  - updated ujson from 1.35 to 5.4.0  - updated prometheus_client to 0.16.0  - resolved app.ctx issues  - resolved event loop issue.

--- setup.py
+++ setup.py
@@ -6,17 +6,18 @@ setup(
     name='sanic-prometheus',
     version=f'{get_version()}',
     description='Exposes Prometheus monitoring metrics of Sanic apps.',
-    url='http://github.com/dkruchinin/sanic-prometheus',
-    author='Dan Kruchinin',
-    author_email='dan.kruchinin@gmail.com',
+    url='http://github.com/AltanAlpay/sanic-prometheus',
+    author='Dan Kruchinin, Altan Alpay',
+    author_email='dan.kruchinin@gmail.com, altan.alpay@gmail.com',
     license='MIT',
     packages=['sanic_prometheus'],
     zip_safe=False,
     platforms='any',
     install_requires=[
-        'sanic>=18.12',
-        'prometheus-client~=0.7.1',
-        'psutil>=5.2.0'
+        'sanic>=22.12.0',
+        'prometheus-client~=0.16.0',
+        'psutil>=5.6.6'
+        'ujson>=5.4.0'
     ],
     classifiers=[
         'Intended Audience :: Developers',
@@ -29,8 +30,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
 
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.11'
     ],
     keywords='sanic prometheus monitoring'
 )
